0535eef3b88111e925e6308aa4560906c9494202
Francis Delorenzi <fdeloren@lakeheadu.ca>
2018-10-17 13:19:50 -0400

    Added header template and login page

--- hello.py
+++ hello.py
@@ -1,13 +1,39 @@
-from flask import Flask
+from functools import wraps
+import json
+import gc
+import sys
+import traceback
+
+from flask import Flask, render_template, request, url_for, redirect, session, flash, jsonify
+
+import csv
+
 app = Flask(__name__)
 
+<<<<<<< HEAD
 # comment to test git 
 # test 2
+=======
+@app.errorhandler(400)
+def four_hundred_err(e):
+    return render_template('400.html', error=e)
+>>>>>>> Added header template and login page
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html')
+
+
+@app.errorhandler(500)
+def five_hundred_err(e):
+    return render_template('500.html', error=e)
+
+# @app.route('/')
+# def main():
+#     return render_template('main.html')
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
-@app.route('/page2')
-def new_temp():
-    return 'WTF'
+@app.route('/login')
+def login():
+    return render_template('login.html')
