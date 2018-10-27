df92746248f80ef3f2c7d5f8eac566898ecfcbfb
Peter Marinis <peter.marinis96@gmail.com>
2018-10-27 15:04:58 -0400

    test

--- hello.py
+++ hello.py
@@ -6,7 +6,6 @@ import traceback
 
 from flask import Flask, render_template, request, url_for, redirect, session, flash, jsonify
 
-<<<<<<< HEAD
 user_dict = { 
   '1': {
     'username': 'ABC',
@@ -22,34 +21,16 @@ app = Flask(__name__)
 
 @app.errorhandler(400)
 def four_hundred_err(e):
-  return render_template('400.html', error=e)
-=======
-import csv
-
-app = Flask(__name__)
-
-<<<<<<< HEAD
-# comment to test git 
-# test 2
-=======
-@app.errorhandler(400)
-def four_hundred_err(e):
     return render_template('400.html', error=e)
->>>>>>> 3c9ce7cc27803c0fc65b4eab5786b1b6f2a17456
 
 
 @app.errorhandler(404)
 def page_not_found(e):
-<<<<<<< HEAD
   return render_template('404.html')
-=======
-    return render_template('404.html')
->>>>>>> 3c9ce7cc27803c0fc65b4eab5786b1b6f2a17456
 
 
 @app.errorhandler(500)
 def five_hundred_err(e):
-<<<<<<< HEAD
   return render_template('500.html', error=e)
 
 @app.route('/')
@@ -77,16 +58,4 @@ def login():
 @app.route('/main')
 def mainpage():
   return render_template('main.html')
-=======
-    return render_template('500.html', error=e)
 
-# @app.route('/')
-# def main():
-#     return render_template('main.html')
->>>>>>> origin/master
-
-@app.route('/')
-@app.route('/login')
-def login():
-    return render_template('login.html')
->>>>>>> 3c9ce7cc27803c0fc65b4eab5786b1b6f2a17456
